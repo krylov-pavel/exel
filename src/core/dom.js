9dc3ec60092d6530f8b61767f32048291c6adb2b
Павел Крылов <pavel.krylov@henderson.ru>
2021-06-29 08:49:06 +0300

    finish base table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -85,9 +85,11 @@ class Dom {
   }
   addClass(className) {
     this.$el.classList.add(className)
+    return this
   }
   removeClass(className) {
     this.$el.classList.remove(className)
+    return this
   }
 }
 export function $(selector) {
